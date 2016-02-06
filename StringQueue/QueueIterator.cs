2bc5c0987a5e1fa77269b570efd828031d3d7c61
Наташа Неровня <natanerovnja@gmail.com>
2016-02-06 12:22:30 +0300

    Add iterator

---
+++ StringQueue/QueueIterator.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    internal class QueueIterartor : IEnumerator
+    {
+        public string[] array;
+        private int position = -1;
+
+        public QueueIterartor(string[] list)
+        {
+            array = list;
+        }
+
+        public bool MoveNext()
+        {
+            if (position == array.Length - 1)
+            {
+                Reset();
+                return false;
+            }
+            position++;
+            return true;
+        }
+
+        public void Reset()
+        {
+            position = -1;
+        }
+
+        object IEnumerator.Current
+        {
+            get { return array[position]; }
+        }
+
+        public string Current
+        {
+            get { return array[position]; }
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
